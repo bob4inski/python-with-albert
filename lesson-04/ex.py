4ae562438de734471d0490b576ea7855d31aa49c
b0b4inski <robertstryzhak@gmail.com>
2022-02-21 13:28:31 +0300

    4 yrok

---
+++ lesson-04/ex.py
@@ -0,0 +1,30 @@
+from operator import truediv
+import os
+def da():
+    a = os.uname()
+    return a.sysname == 'Linux'
+
+a = os.uname()
+
+
+def res(n,a,b):
+    if os.path.exists(n) and os.path.isfile(n):
+        
+        if n[-4:] == '.txt':
+            filee = open(n,'r')
+
+            d = filee.read()
+            print(d)
+            d = d.replace(a,b)
+            print(d)
+            filee.close()
+            filee = open(n,'w')
+            filee.write(d)
+            
+            filee.close()
+    
+
+            
+
+res('ad.txt','cat','dog')
+print(os.path.exists('ad.txt'))
